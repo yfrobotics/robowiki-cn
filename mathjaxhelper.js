f5a8c9d31acd4609537d606a098f44c9f9c09de8
automaticdai <automatic.dai@gmail.com>
2020-10-20 21:45:53 +0100

    deployed on 2020-10-20 21:45:53 by yfrl

---
+++ mathjaxhelper.js
@@ -0,0 +1,14 @@
+MathJax.Hub.Config({tex2jax: {inlineMath: [['$','$'], ['\\(','\\)']]}});
+
+window.MathJax = {
+  tex: {
+    inlineMath: [["\\(", "\\)"]],
+    displayMath: [["\\[", "\\]"]],
+    processEscapes: true,
+    processEnvironments: true
+  },
+  options: {
+    ignoreHtmlClass: ".*|",
+    processHtmlClass: "arithmatex"
+  }
+};
